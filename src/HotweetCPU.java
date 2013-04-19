798c044ee4063741e711b1941d0f710377640ba4
Takumi Kadowaki <hourou.hamurinzu@gmail.com>
2013-04-19 12:50:36 +0900

    エラーファイルの修正

--- src/HotweetCPU.java
+++ src/HotweetCPU.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -57,61 +57,3 @@ public class HotweetCPU {
 		}
 	}
 }
-=======
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
-
-
-public class HotweetCPU {
-	
-	public static void printAll(CPU cpu) {
-		
-		// Mapからiteratorを取得
-		Map<String, ArrayList<TemperatureData>> map = cpu.getMap();
-		Set<Entry<String, ArrayList<TemperatureData>>> set = map.entrySet();
-		Iterator<Entry<String, ArrayList<TemperatureData>>> iterator = set.iterator();
-		
-		while(iterator.hasNext()) {
-			Entry<String, ArrayList<TemperatureData>> entry = iterator.next();
-			String key = entry.getKey();
-			System.out.printf("========== %s ==========\n", key);
-			Iterator<TemperatureData> iterator2 = entry.getValue().iterator();
-			while (iterator2.hasNext()) {
-				TemperatureData data = iterator2.next();
-				System.out.println(data.getDate() + " : " + data.getTemperature() + "°C");
-			}
-			System.out.println('\n');
-		}
-		System.out.println("============================\n");
-		
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
-		try {
-			CPU cpu = new CPU();
-			
-			for(int i = 0; i < 60; i++) {
-				cpu.update();
-				Thread.sleep(1000);
-			}
-			
-			
-		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-	}
-}
->>>>>>> temp1
