bd40ba5dee9c02fb8147ac594ccdcbc4a11f9b1e
Takumi Kadowaki <hourou.hamurinzu@gmail.com>
2013-04-22 16:16:49 +0900

    TaskTrayサンプルの追加

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,39 @@
-
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Test {
 
-	/**
-	 * @param args
-	 * @throws Exception
-	 */
 	public static void main(String[] args) throws Exception {
+
+		if (!SystemTray.isSupported()) {
+			System.out.println("SystemTray is not supported!");
+			return;
+		}
+		SystemTray tray = SystemTray.getSystemTray();
+		Image image = Toolkit.getDefaultToolkit().getImage("test2.png");
+
+		PopupMenu popup = new PopupMenu();
+		MenuItem defaultItem = new MenuItem("mustang");
+		defaultItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				System.out.println("popup action");
+			}
+		});
+		popup.add(defaultItem);
+
+		TrayIcon trayIcon = new TrayIcon(image, "Tray Demo", popup);
+		trayIcon.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				System.out.println("trayIcon action");
+			}
+		});
+		tray.add(trayIcon);
 
 	}
 
